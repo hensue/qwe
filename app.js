20629999f59ee175b0926f159bf2aca37bce285c
hensue <138180419+hensue@users.noreply.github.com>
2023-10-30 04:52:53 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,31 +1,68 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
+// const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const path = require("path");
+const multer = require('multer');
+
+const keys = require("./config/keys");
+
+const cors = require("cors");
+
+app.use(cors());
+
+// mongoose.Promise = global.Promise;
+
+// mongoose.connect(
+//   "mongodb://localhost:27017",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   },
+//   (err) => {
+//     if (err) throw err;
+//     console.log("Connected to MongoDB!!!");
+//   }
+// );
+
+const storage = multer.diskStorage({
+  destination: 'uploads/',
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+const upload = multer({storage});
+
+app.use('/api/upload', upload.array('files'), (req, res)=>{
+
+})
+
+app.use('/api/download/:filename', (req, res) => {
+  const filename = req.params.filename;
+  const filePath = path.join(__dirname, 'uploads', filename); 
+
+  res.download(filePath, (err) => {
+    if (err) {
+      res.status(500).send('Error downloading file');
+    }
+  });
+});
+
+
+app.use(bodyParser.json());
+
+app.use("/api/cars/", require("./routes/carsRoute"));
+app.use("/api/users/", require("./routes/usersRoute"));
+app.use("/api/bookings/", require("./routes/bookingsRoute"));
+
+// app.use(express.static("public"))
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 
-app.get("/api", (_req, res) => {
-  res
-    .status(200)
-    .json({ success: true, message: "Server is runing on Vercel" });
+
+app.use("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
 });
 
-app.use("*", (_req, res) => {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
-
-app.use((_req, res, next) => {
-  res.status(404).json({
-    message:
-      "Ohh you are lost, read the API documentation to find your way back home :)",
-  });
-});
-app.use((err, _req, res, _next) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode);
-  res.json({
-    message: err.message,
-  });
-});
 module.exports = app;
